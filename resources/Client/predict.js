cf0eef0c284e96a2cb192801f0e096566112cdcf
ondemand2002 <50772841+ondemand2002@users.noreply.github.com>
2019-09-18 16:56:01 +1000

    upload local repository to github

---
+++ resources/Client/predict.js
@@ -0,0 +1,55 @@
+$(function () {
+
+    var url = "https://southeastasia.api.cognitive.microsoft.com/customvision/v3.0/Prediction/fd6cd228-87be-4c7e-b1ac-8ae43ba5f222/classify/iterations/Iteration1/image";
+    var predictionKey = "346e1c74d3f6448aabec2a7cdfb26b75";
+    
+    var fs = require("fs");
+    var _ = require('underscore');
+
+    // Store the value of a selected image for display
+    var imageBytes;
+
+    // Handle clicks of the Upload Image button
+    $("#select_button").click(function () {
+
+        $('#analysisResults').html('');
+
+        // Load the image and show it in the window
+        const electron = require('electron');
+        const dialog = require('electron').dialog;
+
+        var va = electron.remote.dialog.showOpenDialog();
+
+        var contents = fs.readFileSync(va[0], "base64");
+        imageBytes = fs.readFileSync(va[0]);
+
+        $('#previewImage').html('<img width="240" src="data:image/png;base64,' + contents + '" />');
+
+        // Submit the image to the Custom Vision Service
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: imageBytes,
+            processData: false,
+            headers: {
+                "Prediction-Key": predictionKey,
+                "Content-Type": "application/octet-stream"
+            }
+        }).done(function (data) {
+            var predictions = data.predictions;
+            var solarcell = [predictions.find(o => o.tagName === 'solarcell-bad'), predictions.find(o => o.tagName === 'solarcell-good')];
+            var sortedsolarcell = _.sortBy(solarcell, 'probability').reverse();
+            var possiblesolarcell = sortedsolarcell[0];
+
+            if (possiblesolarcell.probability > 0.9) {
+                $('#analysisResults').html('<div class="matchLabel">' + possiblesolarcell.tagName + ' (' + (possiblesolarcell.probability * 100).toFixed(0) + '%)' + '</div>');
+            }
+            else {
+                $('#analysisResults').html('<div class="noMatchLabel">Unknown picture</div>');
+            }
+
+        }).fail(function (xhr, status, err) {
+            alert(err);
+        });
+    });
+});
